641d3a0f2544873a3b47ad6e908b0b898886c86a
Delos-343 <mfachry.handoko@gmail.com>
2023-11-25 12:13:00 +0700

    Generated encryption keys

--- keys.js
+++ keys.js
@@ -2,27 +2,19 @@
 import { randomBytes } from 'crypto';
 
 // (32 bytes = 256 bits)
-const aesEncryptionKey = () => {
-    return randomBytes(32).toString('hex');
-};
+const aesEncryptionKey = randomBytes(32).toString('hex');
 console.log('AES Encryption Key:', aesEncryptionKey);
 
 // (16 bytes = 128 bits)
-const aesEncryptionIV  = () => {
-  return randomBytes(16).toString('hex');
-};
+const aesEncryptionIV = randomBytes(16).toString('hex');
 console.log('AES Encryption IV:', aesEncryptionIV);
 
 
 // (8 bytes = 64 bits)
-const desEncryptionIV  = () => {
-    return randomBytes(8).toString('hex');
-};
+const desEncryptionIV = randomBytes(8).toString('hex');
 console.log('DES Encryption IV:', desEncryptionIV);
 
 
 // (16 bytes = 128 bits)
-const rc4EncryptionIV  = () => {
-    return randomBytes(16).toString('hex');
-};
+const rc4EncryptionIV = randomBytes(16).toString('hex');
 console.log('DES Encryption IV:', rc4EncryptionIV);
