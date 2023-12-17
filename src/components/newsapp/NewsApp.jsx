626ca62e6e667fe364f2af3f77069cfdf9a980ea
Delos-343 <mfachry.handoko@gmail.com>
2023-12-17 19:06:17 +0800

    No unused var/ref:undefined

--- src/components/newsapp/NewsApp.jsx
+++ src/components/newsapp/NewsApp.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
