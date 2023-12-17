e34551e54261ff77889007762a623aa6a679eda3
Delos-343 <mfachry.handoko@gmail.com>
2023-12-17 19:50:09 +0800

    Debugged basic unused/undefined

--- src/components/newsapp/EditNews.jsx
+++ src/components/newsapp/EditNews.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useNavigate, useParams } from "react-router-dom";
